86129bbb4ce9415ba9039335a5eea96834835d52
dylan momplaisir <dmomplaisir@icloud.com>
2018-08-10 23:23:05 -0400

    Deleted the vue front-end

--- client/components/Header.vue
+++
@@ -1,7 +0,0 @@
-<template>  
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-      <router-link tag="a" class="navbar-brand" to="/" exact>Deliveroo clone</router-link>
-    </div>
-  </nav>
-</template> 
