8eb8365f866e0f18a7733813ae0f9bedfa39a373
dylan momplaisir <dmomplaisir@icloud.com>
2018-08-30 02:55:12 -0400

    Cleaned AmherstStudent links

--- client/components/mediacard3.js
+++ client/components/mediacard3.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import Card from './card.js'
 import Container from './container.js'
 
-let LOCAL_URI = 'http://api.amherststudent.com'
+let LOCAL_URI = 'api.amherststudent.com'
 const MediaCard3 = (props) => (
   <Card className={`thrds ${ props.className }`}>
     <div style={ Boolean(props.article.featuredImage)  ? {backgroundImage: `url('${LOCAL_URI + props.article.featuredImage.url}')`} : {minHeight:'60px'} } className="cardHeader">
