1eb1b893e8b4281813cbb5e7fa2c15a315c52a17
dylan momplaisir <dmomplaisir@icloud.com>
2018-08-30 03:18:30 -0400

    Cleaned category header

--- client/components/category_header.js
+++ client/components/category_header.js
@@ -5,26 +5,33 @@ import Card from './card'
 const Category_Header = (props) => (
   <Card className="ting margin-20">
     <div className="header">
-      <h1>{props.type.toUpperCase()}  </h1>
+      <div className="bottom_Line">
+      <h1>{props.type.toUpperCase()} <br />  </h1>
+      </div>
     </div>
       <style jsx>{`
         h1{
           /* NEWS: */
           font-family: "adobe-garamond-pro",serif;
-          font-size: 64px;
+          font-size: 2.5em;
           color: #FFFFFF;
           letter-spacing: 0.34px;
           text-align: right;
+          padding: 0;
+          margin: 0 auto;
+          bottom: 0;
+          padding-top: 10px;
+          padding-bottom: 10px;
+        }
+        .bottom_Line{
           background: rgba(0,0,0,0.7);
           position: absolute;
           width: 100%;
           bottom: 0;
           margin: 0;
-          height:auto;
-          padding-bottom: 20px;
-          padding-top: 10px;
-          padding-right: 20px;
 
+
+          padding-right: 20px;
         }
         .header{
           background-image: url('https://assets.forwardcdn.com/images/cropped/amherst-college-buildings-img-6512-1500990972.JPG');
