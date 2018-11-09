17e495b70a8ac777c94b700ab580433f170cf512
dylan momplaisir <dmomplaisir@icloud.com>
2018-11-09 03:48:25 -0500

    added prettier, and a code of conduct:

--- client/components/article_core.js
+++ client/components/article_core.js
@@ -1,42 +1,48 @@
-import React from 'react'
-import Card from './card'
-import Container from './container'
-import showdown from 'showdown'
+import React from "react";
+import Card from "./card";
+import Container from "./container";
+import showdown from "showdown";
 
 function createMarkup(content) {
-  var formatted_content = content.replace(/(?:\r\n|\r|\n)/g, '<br >')
-  var converter = new showdown.Converter()
-  var html = converter.makeHtml(content).replace(/(?:\r\n|\r|\n)/g, '<br >')
-  return { __html: html }
+  var formatted_content = content.replace(/(?:\r\n|\r|\n)/g, "<br >");
+  var converter = new showdown.Converter();
+  var html = converter.makeHtml(content).replace(/(?:\r\n|\r|\n)/g, "<br >");
+  return { __html: html };
 }
 
 const Article_Core = props => (
   <Card className="margin-20">
     <Container>
       <div>
-        <div className="article_text"  dangerouslySetInnerHTML={createMarkup(props.article.content)} />
-          <style dangerouslySetInnerHTML={{__html: `
+        <div
+          className="article_text"
+          dangerouslySetInnerHTML={createMarkup(props.article.content)}
+        />
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
         img  { width: 100%; height: auto; }
-      `}} />
+      `
+          }}
+        />
       </div>
     </Container>
     <style jsx>
       {`
-        .article_text  {
-          margin : 25px 10px 25px 10px;
+        .article_text {
+          margin: 25px 10px 25px 10px;
         }
         .article_text > p {
-         font-size: 110px;
+          font-size: 110px;
         }
 
-        img{
+        img {
           width: 100%;
-          height:auto;
+          height: auto;
         }
-
       `}
     </style>
   </Card>
-)
+);
 
-export default Article_Core
+export default Article_Core;
