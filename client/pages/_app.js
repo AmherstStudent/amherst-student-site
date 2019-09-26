eec5db2ad77c10baf36c15795b0b3c1de16be526
DMomplaisirASC <dmomplaisir@icloud.com>
2019-09-26 17:17:39 -0400

    Updated _app.js

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -5,6 +5,15 @@ import { DefaultSeo } from 'next-seo'
 import SEO from '../lib/nextseo.js'
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+ 
+    return { pageProps };
+  }
+  
   render() {
     const { Component, pageProps, apollo } = this.props
     return (
